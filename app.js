2534c960a7f019d2b337a14ad230d2f1eecfbb62
Dmitry Bessonov <me@mgdz.su>
2021-02-12 13:25:17 +0700

    code cleanup /

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-'use strict';
+//'use strict';
 
 const express = require('express');
 const ejs = require('ejs');
@@ -13,16 +13,9 @@ const routes = require('./routes');
 const config = require('./config');
 const mqtt = require('mqtt');
 const device = require('./device');
-const fs = require('fs');
+
 const app = express();
-// const https = require('https');
-// const privateKey = fs.readFileSync(config.https.privateKey, 'utf8');
-// const certificate = fs.readFileSync(config.https.certificate, 'utf8');
-// const credentials = {
-//    key: privateKey,
-//    cert: certificate
-// };
-// const httpsServer = https.createServer(credentials, app);
+
 global.devices = [];
 
 if (config.devices) {
@@ -59,6 +52,7 @@ app.use(passport.session());
 
 // Passport configuration
 require('./auth');
+
 app.get('/', routes.site.index);
 app.get('/login', routes.site.loginForm);
 app.post('/login', routes.site.login);
@@ -78,15 +72,6 @@ app.post('/provider/v1.0/user/unlink', routes.user.unlink);
 // httpsServer.listen(config.https.port);
 app.listen(config.http.port, config.http.host);
 
-function findDevIndex(arr, elem) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i].type === elem) {
-      return i;
-    }
-  }
-  return false;
-}
-
 
 const statPairs = [];
 
@@ -100,12 +85,8 @@ global.devices.forEach((device) => {
       console.log(JSON.stringify(capability));
       if (capability.retrievable) {
         const statTopic = `${prefix + deviceId}/${capability.type}/${capability.state.instance}`;
-        const statType = capability.type;
-        const statInstance = capability.state.instance;
         statPairs.push({
           deviceId,
-          type: statType,
-          instance: statInstance,
           topic: statTopic,
           capability,
         });
@@ -117,12 +98,8 @@ global.devices.forEach((device) => {
       console.log(JSON.stringify(property));
       if (property.retrievable) {
         const statTopic = `${prefix + deviceId}/${property.type}/${property.state.instance}`;
-        const statType = property.type;
-        const statInstance = property.state.instance;
         statPairs.push({
           deviceId,
-          type: statType,
-          instance: statInstance,
           topic: statTopic,
           property,
         });
@@ -142,133 +119,22 @@ if (statPairs) {
         if (device.data.id === statPairs[matchedDeviceId].deviceId) {
           console.log(statPairs[matchedDeviceId]);
           if (statPairs[matchedDeviceId].capability) {
-            const capability = device.data.capabilities.findIndex(c => c.type === statPairs[matchedDeviceId].capability.type);
-            device.data.capabilities[capability].state.instance = statPairs[matchedDeviceId].instance;
-            device.data.capabilities[capability].state.value = JSON.parse(message);
+            device.data.capabilities.forEach((capability, i) => {
+              if (capability.state.instance === statPairs[matchedDeviceId].capability.state.instance) {
+                device.data.capabilities[i].state.value = JSON.parse(message);
+              }
+            });
           }
           if (statPairs[matchedDeviceId].property) {
-            console.log('PROPERTY');
             device.data.properties.forEach((property, i) => {
-              console.log(property);
-              console.log(statPairs[matchedDeviceId].instance);
-              if (property.state.instance === statPairs[matchedDeviceId].instance) {
-                
+              if (property.state.instance === statPairs[matchedDeviceId].property.state.instance) {
                 device.data.properties[i].state.value = JSON.parse(message);
               }
-            })
-//            const property = device.data.properties.instance.findIndex(p => p.type === statPairs[matchedDeviceId].property.type);
-//            device.data.properties[property].state.instance = statPairs[matchedDeviceId].instance;
-//            device.data.properties[property].state.value = JSON.parse(message);
+            });
             console.log(JSON.stringify(device.data));
           }
         }
       });
-/*
-      const device = global.devices.find((device) => device.data.id === statPairs[matchedDeviceId].deviceId);
-      var devindx;
-      switch (statPairs[matchedDeviceId].topicType) {
-        case 'on':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.on_off')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = ['on', '1', 'true'].includes(message.toString().toLowerCase());
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'mute':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.toggle')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = ['on', '1', 'true'].includes(message.toString().toLowerCase());
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'hsv':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.color_setting')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = JSON.parse(message);
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'rgb':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.color_setting')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = JSON.parse(message);
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'temperature_k':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.color_setting')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = JSON.parse(message);
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'thermostat':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.mode')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = JSON.parse(message);
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'fan_speed':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.mode')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = JSON.parse(message);
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'brightness':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.range')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = JSON.parse(message);
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'temperature':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.range')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = JSON.parse(message);
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'volume':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.range')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = JSON.parse(message);
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        case 'channel':
-          try {
-            devindx = findDevIndex(device.data.capabilities, 'devices.capabilities.range')
-            device.data.capabilities[devindx].state.instance = statPairs[matchedDeviceId].topicType;
-            device.data.capabilities[devindx].state.value = JSON.parse(message);
-          } catch (err) {
-            console.log(err);
-          }
-          break;
-        default:
-          console.log('Unknown topic Type: ' + statPairs[matchedDeviceId].topicType);
-      } */
     });
   });
 
