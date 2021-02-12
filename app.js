3f4aec0ba66e8e42cff576891a31fdb609e55f4b
Dmitry Bessonov <me@mgdz.su>
2021-02-12 12:09:36 +0700

    property

--- app.js
+++ app.js
@@ -147,6 +147,7 @@ if (statPairs) {
             device.data.capabilities[capability].state.value = JSON.parse(message);
           }
           if (statPairs[matchedDeviceId].property) {
+            console.log('PROPERTY');
             const property = device.data.properties.findIndex(p => p.type === statPairs[matchedDeviceId].property.type);
             device.data.properties[property].state.instance = statPairs[matchedDeviceId].instance;
             device.data.properties[property].state.value = JSON.parse(message);
